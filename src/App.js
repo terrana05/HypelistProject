54543cbc1db859784e4c7ecb29129fd5278ebfa7
terrana05 <73558331+terrana05@users.noreply.github.com>
2021-12-08 18:17:47 -0500

    Finish Hypelist Project

--- src/App.js
+++ src/App.js
@@ -1,145 +1,233 @@
 import "./styles.css";
-//Write array below
-
-// let people = [
-//   { firstName: "Omari", lastName: "Prass" },
-//   { firstName: "Melody", lastName: "Suero" },
-//   { firstName: "Groana", lastName: "Melendez" },
-//   { firstName: "Maxwell", lastName: "Williams" }
-// ];
 
 export default function App() {
-  //   let firstNames = [
-  //     "Omari",
-  //     "Melody",
-  //     "Keturah",
-  //     "Aldo",
-  //     "Reginea",
-  //     "Amori",
-  //     "Laurent",
-  //     "Ye",
-  //     "Matt",
-  //     "Judy",
-  //     "Sami",
-  //     "Maxwell",
-  //     "Groana",
-  //     "Aland",
-  //     "Jean",
-  //     "Lorena"
-  //   ];
-
   let people = [
     {
       firstName: "Omari",
       lastName: "Prass",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "#1fb752",
+      yearBorn: "2004",
+      monthBorn: "April",
+      countrytoVisit: "Japan",
+      dogorCat: "Both",
+      foodLove: "Pizza",
+      hobby: "Playing Video Games",
+      movieShow: "Gumball, Regular Show",
+      career: "Website Developer"
     },
     {
       firstName: "Melody",
       lastName: "Suero",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "#c8a2c8",
+      yearBorn: "2003",
+      monthBorn: "May",
+      countrytoVisit: "Japan",
+      dogorCat: "Both",
+      foodLove: "Everything",
+      hobby: "Drawing",
+      movieShow: "Courage the Cowardly Dog",
+      career: "Tech/Art/Fashion"
     },
     {
       firstName: "Keturah",
       lastName: "Allen",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: undefined,
+      yearBorn: "2005",
+      monthBorn: "March",
+      countrytoVisit: "Boar Boar",
+      dogorCat: "Dog",
+      foodLove: "Everything",
+      hobby: "Writing",
+      movieShow: "Not Sure",
+      career: "Build/Program Machines"
     },
     {
       firstName: "Aldo",
       lastName: "Terrana",
       borough: "New Jersey",
-      favColor: ""
+      favColor: "red",
+      yearBorn: "2005",
+      monthBorn: "September",
+      countrytoVisit: "Japan",
+      dogorCat: "Dog",
+      foodLove: "Sushi",
+      hobby: "Ice Skating",
+      movieShow: "You",
+      career: "Geneticist"
     },
     {
       firstName: "Reginea",
       lastName: "Walker",
       borough: "Queens",
-      favColor: ""
+      favColor: " #eb7d34",
+      yearBorn: "2005",
+      monthBorn: "August",
+      countrytoVisit: "Not Japan",
+      dogorCat: "Dog",
+      foodLove: "Pizza",
+      hobby: "Writing",
+      movieShow: "Moana",
+      career: "Fashion Designer"
     },
     {
       firstName: "Amori",
       lastName: "Steele",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "#A45ee5",
+      yearBorn: "2004",
+      monthBorn: "June",
+      countrytoVisit: "Norway",
+      dogorCat: "Dog",
+      foodLove: "Burger",
+      hobby: "Video Games",
+      movieShow: "ED, edd n eddy",
+      career: "Software Engineer"
     },
     {
       firstName: "Laurent",
       lastName: "Benjamin",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "red",
+      yearBorn: "2004",
+      monthBorn: "November",
+      countrytoVisit: "England",
+      dogorCat: "Both",
+      foodLove: "Legumes",
+      hobby: "Reading",
+      movieShow: "N/A",
+      career: "Software Engineer"
     },
     {
-      firstName: "Ye",
+      firstName: "Ye ☜(ﾟヮﾟ☜)",
       lastName: "(☞ﾟヮﾟ)☞Paing",
-      borough: "Queens",
-      favColor: ""
+      borough: "Queens ☜(ಠ_ಠ☜)",
+      favColor: "#45FCB4",
+      yearBorn: "1989 (╯°□°)╯︵ ┻━┻",
+      monthBorn: "July",
+      countrytoVisit: "Japan",
+      dogorCat: "Both",
+      foodLove: "Thai",
+      hobby: "Handball",
+      movieShow: "The Matrix",
+      career: "Software Engineer"
     },
     {
       firstName: "Matt",
       lastName: "Schupack",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "bisque",
+      yearBorn: "1994",
+      monthBorn: "October",
+      countrytoVisit: "Spain",
+      dogorCat: "Cat",
+      foodLove: "Sushi",
+      hobby: "Baking Bread",
+      movieShow: "Endeavour",
+      career: "Software Engineer"
     },
     {
       firstName: "Judy",
       lastName: "Truong",
       borough: "Brooklyn",
-      favColor: ""
+      favColor: "#cd94f2",
+      yearBorn: "1991",
+      monthBorn: "January",
+      countrytoVisit: "Korea",
+      dogorCat: "Dog",
+      foodLove: "Ramen",
+      hobby: "Reading",
+      movieShow: "The Mummy",
+      career: "Software Engineer"
     },
     {
       firstName: "Sami",
       lastName: "Lieberman",
       borough: "Manhattan",
-      favColor: ""
+      favColor: "#00d0ff",
+      yearBorn: "1997",
+      monthBorn: "May",
+      countrytoVisit: "Africa",
+      dogorCat: "Both",
+      foodLove: "Sushi",
+      hobby: "Tennis",
+      movieShow: "Modern Family",
+      career: "Software Engineer"
     },
     {
       firstName: "Maxwell",
       lastName: "Williams",
       borough: "New Jersey",
-      favColor: ""
+      favColor: "#ebba34",
+      yearBorn: "1992",
+      monthBorn: "February",
+      countrytoVisit: "France",
+      dogorCat: "Dog",
+      foodLove: "Burger and Fries",
+      hobby: "Movies",
+      movieShow: "Pan's L",
+      career: "Teacher"
     },
     {
       firstName: "Groana",
       lastName: "Melendez",
       borough: "Bronx",
-      favColor: ""
+      favColor: "#40e0d0",
+      yearBorn: "1984",
+      monthBorn: "June",
+      countrytoVisit: "Ghana",
+      dogorCat: "Dog",
+      foodLove: "Ice Cream",
+      hobby: "Photography",
+      movieShow: "The Haunting at...",
+      career: "Software Engineer"
     },
-    { firstName: "Aland", lastName: "Kuang", borough: "Phoenix", favColor: "" },
+    {
+      firstName: "Aland",
+      lastName: "Kuang",
+      borough: "Phoenix",
+      favColor: "#000AD4",
+      yearBorn: "1994",
+      monthBorn: "February",
+      countrytoVisit: "Japan",
+      dogorCat: "Dog",
+      foodLove: "Dumplings",
+      hobby: "Editing Writing Pieces",
+      movieShow: "Rick and Morty",
+      career: "Software Engineer"
+    },
     {
       firstName: "Jean",
       lastName: "Saldarriaga",
       borough: "Manhattan",
-      favColor: ""
+      favColor: "#2a693b",
+      yearBorn: "2005",
+      monthBorn: "January",
+      countrytoVisit: "Japan",
+      dogorCat: "Dog",
+      foodLove: "Lasagna",
+      hobby: "Video Games",
+      movieShow: "Scott Pilgrim Vs. The World",
+      career: "Software Engineer"
     },
     {
       firstName: "Lorena",
       lastName: "Castillo",
       borough: "Manhattan",
-      favColor: ""
+      favColor: undefined,
+      yearBorn: "2003",
+      monthBorn: "May",
+      countrytoVisit: "Korea",
+      dogorCat: "Dog",
+      foodLove: "Lo Mein",
+      hobby: "Volleyball",
+      movieShow: "Reign",
+      career: "Digital Forensic"
     }
   ];
 
-  // let lastNames = [
-  //   "Prass",
-  //   "Suero",
-  //   "Allen",
-  //   "Terrana",
-  //   "Walker",
-  //   "Steele",
-  //   "Benjamin",
-  //   "(☞ﾟヮﾟ)☞Paing",
-  //   "Schupack",
-  //   "Truong",
-  //   "Lieberman",
-  //   "Williams",
-  //   "Melendez",
-  //   "Kuang",
-  //   "Saldarriaga",
-  //   "Castillo"
-  // ];
   return (
     <div className="App">
       <table>
@@ -147,14 +235,36 @@ export default function App() {
           <th>First Name</th>
           <th>Last Name</th>
           <th>Borough</th>
+          <th>Year Born</th>
+          <th>Month Born</th>
+          <th>Country to Visit</th>
+          <th>Dog or Cat?</th>
+          <th>Favorite Food?</th>
+          <th>Hobbies</th>
+          <th>Favorite Movie or Show?</th>
+          <th>Career?</th>
         </tr>
 
         {people.map(function (person, i) {
+          let personBGColor = person.favColor;
+
+          if (person.favColor === undefined) {
+            personBGColor = "grey";
+          }
+
           return (
-            <tr>
+            <tr style={{ backgroundColor: personBGColor }}>
               <td>{person.firstName}</td>
               <td>{person.lastName}</td>
               <td>{person.borough}</td>
+              <td>{person.yearBorn}</td>
+              <td>{person.monthBorn}</td>
+              <td>{person.countrytoVisit}</td>
+              <td>{person.dogorCat}</td>
+              <td>{person.foodLove}</td>
+              <td>{person.hobby}</td>
+              <td>{person.movieShow}</td>
+              <td>{person.career}</td>
             </tr>
           );
         })}
